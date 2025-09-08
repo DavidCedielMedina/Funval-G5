2f8dd4a06ccdd7de15edc45e6795aad0ce4d9b9e
David <jejesus484@email.com>
2025-09-08 01:10:58 -0500

    miniproyecto portafolio actualizado

--- miniproyecto/habilidades.js
+++ miniproyecto/habilidades.js
@@ -1,3 +1,12 @@
-let habilidades=[
-    {nombre: "",nivel:0}
+export let habilidad=[
+    {nombre: "HTML",nivel:90},
+    {nombre: "CSS",nivel:80},
+    {nombre: "JavaScrip",nivel:40},
+    {nombre: "Tailwind",nivel:80},
+    {nombre: "Estilo",nivel:70},
+    {nombre: "Diseño",nivel:80},
+    {nombre: "Creatividad",nivel:80},
+    {nombre: "Funcionalidad",nivel:80},
+    {nombre: "Desarrollo",nivel:80},
+    {nombre: "Adaptavilidad",nivel:100},
 ]
--- miniproyecto/main.js
+++ miniproyecto/main.js
@@ -1,9 +1,11 @@
+import { habilidad } from "./habilidades.js";
 
 let todo = document.querySelector("#todo");
 let botoncito = document.querySelector("#AcceptConditions");
 botoncito.addEventListener("click", function () {
 todo.classList.toggle("dark");
 });
+
 
 let abrircard1=document.querySelector("#open-card1");
 let modalcard1=document.getElementById("card1");
@@ -39,4 +41,45 @@ abrircard3.addEventListener("click",function (){
 
 closecard3.addEventListener("click",function () {
   modalcard3.classList.replace("flex","hidden")
+})
+
+//-----------habilidades---------
+
+let habilidadesList=document.getElementById("habilidadeslist")
+
+for (let index = 0; index < habilidad.length; index++) {
+
+    let nivel=habilidad[index].nivel;
+    let color="";
+
+    switch (true) {
+      case nivel >0 && nivel<=40:
+        color= "bg-gradient-to-r from-red-700 via-red-600 to-red-500";
+        break;
+
+      case nivel>41 && nivel<=70:
+        color= "bg-gradient-to-r from-yellow-700 via-yellow-600 to-yellow-500";
+        break;
+
+      case nivel>71 && nivel<=100:
+        color= "bg-gradient-to-r from-green-700 via-green-600 to-green-500";
+        break;  
+      
+    }
+
+    habilidadesList.innerHTML += `
+          <li class="text-xl">${habilidad[index].nombre}</li>
+          <div class="w-full bg-gray-500/20 rounded-2xl">
+          <div class="w-[${nivel}%] ${color} rounded-2xl px-2">${nivel}%</div>
+        </div>
+        `;
+        console.log(habilidadesList.innerHTML)    
+} //profe aun tengo fallas en esto pero intentare solucionarlo, es trabajo desde 0 profe solo lo idee y lo intente pero me falta
+
+
+let aside=document.getElementById("aside")
+let boton=document.getElementById("botonsito")
+
+boton.addEventListener("click", function () {
+  aside.classList.toggle("hidden")
 })
